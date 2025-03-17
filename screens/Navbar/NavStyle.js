5f8c3cf3fbaba8b4fb11ad828a8986692ae00113
Priyakodam <priya.kodam1997@gmail.com>
2025-03-17 15:22:39 +0530

    Priya

--- screens/Navbar/NavStyle.js
+++ screens/Navbar/NavStyle.js
@@ -1,57 +1,66 @@
-import { StyleSheet, Platform} from "react-native";
+import { StyleSheet } from "react-native";
 import {
   responsiveHeight,
   responsiveWidth,
-  responsiveFontSize,
 } from "react-native-responsive-dimensions";
 
 const styles = StyleSheet.create({
   topBar: {
-    position: "absolute", 
-    top: 15,
+    position: "absolute",
+    top: 0,
     left: 0,
     width: "100%",
-    height: responsiveHeight(10),
+    height: responsiveHeight(11),
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingHorizontal: responsiveWidth(5), 
-    // paddingVertical: responsiveHeight(2.5), 
+    paddingHorizontal: responsiveWidth(5),
     backgroundColor: "white",
     borderBottomWidth: 1,
     borderBottomColor: "#282927",
     zIndex: 1000,
-    elevation: 5, 
-    shadowColor: "#000", 
+    elevation: 5,
+    shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 5,
   },
   logo: {
-    height: responsiveHeight(7), // Adjust height based on screen size
-    width: responsiveWidth(25), // Adjust width based on screen size
+    height: responsiveHeight(15),
+    width: responsiveWidth(25),
     resizeMode: "contain",
+    marginTop: responsiveHeight(2), 
   },
   topRightIcons: {
     flexDirection: "row",
-    gap: responsiveWidth(4), // Dynamic gap
+    gap: responsiveWidth(4),
+    marginTop: responsiveHeight(2),
   },
   glassNavbar: {
     position: "absolute",
-    bottom: responsiveHeight(2), // Responsive bottom spacing
+    bottom: responsiveHeight(2),
     left: "50%",
-    transform: [{ translateX: -responsiveWidth(40) }], // Adjusted for new width
-    width: responsiveWidth(80), // Increased width
+    transform: [{ translateX: -responsiveWidth(40) }],
+    width: responsiveWidth(80),
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
     backgroundColor: "black",
-    padding: responsiveWidth(6), 
-    borderRadius: responsiveWidth(10), 
+    padding: responsiveWidth(6),
+    borderRadius: responsiveWidth(10),
     shadowColor: "#fff",
     shadowOpacity: 0.5,
     shadowRadius: 10,
   },
+  iconWrapper: {
+    width: responsiveWidth(13),
+    height: responsiveWidth(13),
+    borderRadius: responsiveWidth(7),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  activeIconWrapper: {
+    backgroundColor: "white",
+  },
 });
 
 export default styles;
-
